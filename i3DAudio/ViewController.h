39be8d940e36b66882ab1d8430c86909f94a6167
Maurizio Zucchelli <mauzuc90@yahoo.it>
2014-05-10 21:40:30 +0200

    Initialized Xcode project pt. 2

---
+++ i3DAudio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  i3DAudio
+//
+//  Created by Maurizio Zucchelli on 10/05/14.
+//  Copyright (c) 2014 MORZorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
