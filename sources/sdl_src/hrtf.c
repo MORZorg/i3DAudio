59935fd1ea6e3f7d25a20c05430adf3aa18fedfe
Merry38 <rose.tia38@gmail.com>
2014-07-08 17:01:06 +0200

    SDL not needed

--- sources/sdl_src/hrtf.c
+++
@@ -1,9 +0,0 @@
-/*
- * A set of function that have to implement the hrtf
- * to a given audio source.
- * The changes in the source's position should be possible with
- * both a predetermined function and a user-given set of changes
- */
-#include "hrtf.h"
-
-// TODO let me do something!
