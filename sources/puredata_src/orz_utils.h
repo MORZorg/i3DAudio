d84fc87f4a3a11c6f0d3a16db20a31884968bb6e
Maurizio Zucchelli <mauzuc90@yahoo.it>
2014-07-13 16:18:15 +0200

    Added an utility library
    Contains a few functions that should be useful for the implementation

---
+++ sources/puredata_src/orz_utils.h
@@ -0,0 +1,10 @@
+#ifndef __ORZ_UTILS_H__
+#define __ORZ_UTILS_H__
+
+#include <stdlib.h>
+#include <time.h>
+
+int random_int(int, int);
+double nth_element(double*, int, int, int);
+
+#endif // __ORZ_UTILS_H__
