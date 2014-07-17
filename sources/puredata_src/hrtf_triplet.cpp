debae8c52bb8fddd8c9a4643787311006a2a49c7
Merry38 <rose.tia38@gmail.com>
2014-07-17 13:55:20 +0200

    Triplete class complete with calculations

--- sources/puredata_src/hrtf_triplet.cpp
+++ sources/puredata_src/hrtf_triplet.cpp
@@ -3,6 +3,9 @@
 Triplet::Triplet( int* _point_indexes )
 {
 	point_indexes = _point_indexes;
+
+	calculated_inverse = false;
+	H_inverse = NULL;
 }
 
 t_float Triplet::calculate_distance( t_int source_coordinates[ 2 ] )
@@ -22,53 +25,95 @@ t_float Triplet::calculate_distance( t_int source_coordinates[ 2 ] )
 	return distance;
 }
 
-t_float** Triplet::calculate_H_inverse()
-{
-	// Calculating the pseudoinverse of the matrix A, which has the points' coordinates
-	// as columns. The inverse is ( A_t * A ) ^ -1 * A_t
-	t_int A[ 2 ][ 3 ] = {
-		hrtf_coordinates[ point_indexes[ 0 ] ][ 0 ],
-		hrtf_coordinates[ point_indexes[ 1 ] ][ 0 ],
-		hrtf_coordinates[ point_indexes[ 2 ] ][ 0 ];
-		hrtf_coordinates[ point_indexes[ 0 ] ][ 1 ],
-		hrtf_coordinates[ point_indexes[ 1 ] ][ 1 ],
-		hrtf_coordinates[ point_indexes[ 2 ] ][ 1 ] };
-
-	t_int A_t[ 3 ][ 2 ] = {
-		A[ 0 ][ 0 ], A[ 1 ][ 0 ];
-		A[ 0 ][ 1 ], A[ 1 ][ 1 ];
-		A[ 0 ][ 2 ], A[ 1 ][ 2 ] };
-
-	// A_t * A
-	t_int B[ 3 ][ 3 ] = {
-		A_t[ 0 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 0 ],
-		A_t[ 0 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 1 ],
-		A_t[ 0 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 2 ];
-		A_t[ 1 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 0 ],
-		A_t[ 1 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 1 ],
-		A_t[ 1 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 2 ];
-		A_t[ 2 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 0 ],
-		A_t[ 2 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 1 ],
-		A_t[ 2 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 2 ] };
-
-	// Inverse of B
-	t_float inv_det_B = B[ 0 ][ 0 ] * ( B[ 1 ][ 1 ] * B[ 2 ][ 2 ] - B[ 2 ][ 1 ] * B[ 1 ][ 2 ] ) -
-		B[ 0 ][ 1 ] * ( B[ 2 ][ 2 ] * B[ 1 ][ 0 ] - B[ 2 ][ 1 ] * B[ 2 ][ 0 ] ) +
-		B[ 0 ][ 2 ] * ( B[ 1 ][ 0 ] * B[ 2 ][ 1 ] - B[ 1 ][ 1 ] * B[ 2 ][ 0 ] );
-
-	t_float inv_det_B = 1 / inv_det_B;
-
-	t_float B_inv[ 2 ][ 2 ] = {
-		inv_det_B * B[ 1 ][ 1 ], -1 * inv_det_B * B[ 0 ][ 1 ];
-		-1 * inv_det_B * B[ 1 ][ 0 ], inv_det_B * B[ 0 ][ 0 ] }
-
-	t_float pseudoinverse[ 3 ][ 2 ] = {
-		B_inv[ 0 ][ 0 ] * A_t 
-
-
-}
-
 t_float* Triplet::coefficients( t_int source_coordinates[ 2 ] )
 {
-	f_float** H_inverse = calculate_H_inverse();
+	if( !calculated_inverse )
+		calculate_H_inverse();
+
+	f_float* g = malloc( sizeof( t_float ) * 3 );
+
+	g = {
+		H_inverse[ 0 ][ 0 ] * source_coordinates[ 0 ] + H_inverse[ 0 ][ 1 ] * source_coordinates[ 1 ],
+		H_inverse[ 1 ][ 0 ] * source_coordinates[ 0 ] + H_inverse[ 1 ][ 1 ] * source_coordinates[ 1 ],
+		H_inverse[ 2 ][ 0 ] * source_coordinates[ 0 ] + H_inverse[ 2 ][ 1 ] * source_coordinates[ 1 ]
+	};
+
+	return g;
+}
+
+void Triplet::calculate_H_inverse()
+{
+	if( !calculated_inverse )
+	{
+		t_int A[ 2 ][ 3 ] = {
+			{
+				hrtf_coordinates[ point_indexes[ 0 ] ][ 0 ],
+				hrtf_coordinates[ point_indexes[ 1 ] ][ 0 ],
+				hrtf_coordinates[ point_indexes[ 2 ] ][ 0 ]
+			}, {
+				hrtf_coordinates[ point_indexes[ 0 ] ][ 1 ],
+				hrtf_coordinates[ point_indexes[ 1 ] ][ 1 ],
+				hrtf_coordinates[ point_indexes[ 2 ] ][ 1 ]
+			} };
+
+		t_int A_t[ 3 ][ 2 ] = {
+			{ A[ 0 ][ 0 ], A[ 1 ][ 0 ] }
+			{ A[ 0 ][ 1 ], A[ 1 ][ 1 ] }
+			{ A[ 0 ][ 2 ], A[ 1 ][ 2 ] } };
+
+		// A_t * A
+		t_int B[ 3 ][ 3 ] = {
+			{
+				A_t[ 0 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 0 ],
+				A_t[ 0 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 1 ],
+				A_t[ 0 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 0 ][ 1 ] * A[ 1 ][ 2 ]
+			}, {
+				A_t[ 1 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 0 ],
+				A_t[ 1 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 1 ],
+				A_t[ 1 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 1 ][ 1 ] * A[ 1 ][ 2 ]
+			}, {
+				A_t[ 2 ][ 0 ] * A[ 0 ][ 0 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 0 ],
+				A_t[ 2 ][ 0 ] * A[ 0 ][ 1 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 1 ],
+				A_t[ 2 ][ 0 ] * A[ 0 ][ 2 ] + A_t[ 2 ][ 1 ] * A[ 1 ][ 2 ]
+			} };
+
+		// Inverse of B
+		t_float inv_det_B = B[ 0 ][ 0 ] * ( B[ 1 ][ 1 ] * B[ 2 ][ 2 ] - B[ 2 ][ 1 ] * B[ 1 ][ 2 ] ) -
+			B[ 0 ][ 1 ] * ( B[ 2 ][ 2 ] * B[ 1 ][ 0 ] - B[ 2 ][ 1 ] * B[ 2 ][ 0 ] ) +
+			B[ 0 ][ 2 ] * ( B[ 1 ][ 0 ] * B[ 2 ][ 1 ] - B[ 1 ][ 1 ] * B[ 2 ][ 0 ] );
+
+		t_float inv_det_B = 1 / inv_det_B;
+
+		t_float B_inv[ 3 ][ 3 ] = {
+			{
+				inv_det_B * ( B[ 1 ][ 1 ] * B[ 2 ][ 2 ] - B[ 2 ][ 1 ] * B[ 1 ][ 2 ] ),
+				-1 * inv_det_B * ( B[ 1 ][ 0 ] * B[ 2 ][ 2 ] - B[ 2 ][ 0 ] * B[ 1 ][ 2 ] ),
+				inv_det_B * ( B[ 1 ][ 0 ] * B[ 2 ][ 1 ] - B[ 2 ][ 0 ] * B[ 1 ][ 1 ] )
+			}, {
+				-1 * inv_det_B * ( B[ 0 ][ 1 ] * B[ 2 ][ 2 ] - B[ 2 ][ 1 ] * B[ 0 ][ 2 ] ),
+				inv_det_B * ( B[ 0 ][ 0 ] * B[ 2 ][ 2 ] - B[ 2 ][ 0 ] * B[ 0 ][ 2 ] ),
+				-1 * inv_det_B * ( B[ 0 ][ 0 ] * B[ 2 ][ 1 ] - B[ 2 ][ 0 ] * B[ 0 ][ 1 ] )
+			}, {
+				inv_det_B * ( B[ 0 ][ 1 ] * B[ 1 ][ 2 ] - B[ 1 ][ 1 ] * B[ 0 ][ 2 ] ),
+				-1 * inv_det_B * ( B[ 0 ][ 0 ] * B[ 1 ][ 2 ] - B[ 1 ][ 0 ] * B[ 0 ][ 2 ] ),
+				inv_det_B * ( B[ 0 ][ 0 ] * B[ 1 ][ 1 ] - B[ 1 ][ 0 ] * B[ 0 ][ 1 ] )
+			} };
+
+		// B_inverse * A_t
+		H_inverse = malloc( sizeof( t_float ) * 6 );
+		
+		H_inverse = {
+			{
+				B_inv[ 0 ][ 0 ] * A_t[ 0 ][ 0 ] + B_inv[ 0 ][ 1 ] * A_t[ 1 ][ 0 ] + B_inv[ 0 ][ 2 ] * A_t[ 2 ][ 0 ],
+				B_inv[ 0 ][ 0 ] * A_t[ 0 ][ 1 ] + B_inv[ 0 ][ 1 ] * A_t[ 1 ][ 1 ] + B_inv[ 0 ][ 2 ] * A_t[ 2 ][ 1 ]
+			}, {
+				B_inv[ 1 ][ 0 ] * A_t[ 0 ][ 0 ] + B_inv[ 1 ][ 1 ] * A_t[ 1 ][ 0 ] + B_inv[ 1 ][ 2 ] * A_t[ 2 ][ 0 ],
+				B_inv[ 1 ][ 0 ] * A_t[ 0 ][ 1 ] + B_inv[ 1 ][ 1 ] * A_t[ 1 ][ 1 ] + B_inv[ 1 ][ 2 ] * A_t[ 2 ][ 1 ]
+			}, {
+				B_inv[ 2 ][ 0 ] * A_t[ 0 ][ 0 ] + B_inv[ 2 ][ 1 ] * A_t[ 1 ][ 0 ] + B_inv[ 2 ][ 2 ] * A_t[ 2 ][ 0 ],
+				B_inv[ 2 ][ 0 ] * A_t[ 0 ][ 1 ] + B_inv[ 2 ][ 1 ] * A_t[ 1 ][ 1 ] + B_inv[ 2 ][ 2 ] * A_t[ 2 ][ 1 ]
+			} };
+
+		calculated_inverse = true;
+	}
 }
--- sources/puredata_src/hrtf_triplet.hpp
+++ sources/puredata_src/hrtf_triplet.hpp
@@ -8,6 +8,13 @@ class Triplet
 {
 	private:
 		int point_indexes[ 3 ];
+
+		t_float** H_inverse;
+		bool calculated_inverse;
+		
+		// Calculates the pseudoinverse of the matrix A, which has the points' coordinates
+		// as columns. The inverse is ( A_t * A ) ^ -1 * A_t
+		void calculate_H_inverse();
 	
 	public:
 		Triplet( int* );
@@ -17,7 +24,8 @@ class Triplet
 		// As result, the values must be ordered in descending order instead
 		// of ascending one
 		t_float calculate_distance( t_int* );
-		f_float** calculate_H_inverse();
+		// Calculates the g coefficents, either positive or negative (the check must
+		// be performed on the returned value )
 		t_float* coefficients( t_int* );
 };
 
