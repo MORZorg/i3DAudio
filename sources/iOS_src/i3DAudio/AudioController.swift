3048457db54e315cd98d63139e2be4407f12bebb
Maurizio Zucchelli <mauzuc90@yahoo.it>
2014-07-18 14:44:43 +0200

    Integrated libpd inside the app

---
+++ sources/iOS_src/i3DAudio/AudioController.swift
@@ -0,0 +1,29 @@
+//
+//  AudioController.swift
+//  i3DAudio
+//
+//  Created by Maurizio Zucchelli on 18/07/14.
+//  Copyright (c) 2014 MORZorg. All rights reserved.
+//
+
+import UIKit
+
+class AudioController: NSObject {
+    let controller = PdAudioController()
+    
+    init() {
+        super.init()
+        
+        controller.configureAmbientWithSampleRate(44100, numberChannels: 2, mixingEnabled: true)
+        PdBase.setDelegate(self)
+        
+        PdBase.openFile("audio_test.pd", path: NSBundle.mainBundle().resourcePath)
+        controller.active = true
+        
+        controller.print()
+    }
+    
+    func receivePrint(message: String) {
+        println("(pd) \(message)");
+    }
+}
