a11d96d2a65473306db487f070d6944c8797d340
BoreaWolf <ricky.orizio@gmail.com>
2014-07-18 14:59:53 +0200

    Fixed parameters and updated .gitignore.

--- sources/puredata_src/kemar_parameters.hpp
+++ sources/puredata_src/kemar_parameters.hpp
@@ -1,9 +1,9 @@
 #ifndef KEMAR_PARAMETERS
 #define KEMAR_PARAMETERS
 
-// KEMAR HRTF DB specifica parameters
+// KEMAR HRTF DB specific parameters
 #define LEFT_CHANNEL 0
-#define RIGHT_CHANNEL 0
+#define RIGHT_CHANNEL 1
 
 #define SAMPLES_NUMBER 368
 #define SAMPLES_LENGTH 128
